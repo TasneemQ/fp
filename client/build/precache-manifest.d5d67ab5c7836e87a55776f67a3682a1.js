651ec39c8b9c0e6003c0ba588ae9435ffe2f5565
Tasneem Qadeer <tasneem.qadeer@gmail.com>
2019-07-16 13:31:19 -0500

    boilerplate

---
+++ client/build/precache-manifest.d5d67ab5c7836e87a55776f67a3682a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f3b3897d691efdcbeda5",
+    "url": "/static/js/main.697eb53c.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f3b3897d691efdcbeda5",
+    "url": "/static/css/main.48f3edae.chunk.css"
+  },
+  {
+    "revision": "2396245e25b03e9106e022c2ea37fd10",
+    "url": "/index.html"
+  }
+];
